443620cde22a92bb2a1da13ab5ce69121d8be24c
rfkyadtyaa <110508957+rfkyadtyaa@users.noreply.github.com>
2022-08-07 18:51:14 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello World")
+print("Nama saya Rifky")
+print("Umur saya 15 Tahun")
+print("Hobby saya tidur")
+print("Saya tinggal di Mekarsari")
+print("Saya sekolah di SMK Harapan Bangsa")
+print("Saya berasal dari Sekolah SMP Taruna Bhakti Depok")
+print("Saya masuk jurusan TKJ")
+print("Cita-Cita saya ingin menjadi pengusaha sukses")
+print("Saya suka makan sayur")
---
+++ python2.py
@@ -0,0 +1,11 @@
+# Belajar Input
+
+nama = "Rifky" # Rifky adalah Data, nama adalah variabel
+umur = 15 
+Alamat = "Cimanggis,Kota Depok"
+
+print(nama)
+print(umur)
+print(Alamat)
+
+print(" Nama saya adalah " + nama)
