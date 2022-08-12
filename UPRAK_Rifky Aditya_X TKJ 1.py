11a639a1d364b177e0b01656d20e553271304869
rfkyadtyaa <110508957+rfkyadtyaa@users.noreply.github.com>
2022-08-11 20:30:44 -0700

    Add files via upload

---
+++ UPRAK_Rifky Aditya_X TKJ 1.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukkan nama depan: ")
+nama_belakang = input("masukkan nama belakang: ")
+
+print("nama anda adalah", nama_depan, nama_belakang)
+
+celcius = int(input("Masukkan suhu celcius:"))
+ream = (4/5 * celcius) 
+
+print(celcius, "celcius =", ream, "reamur")
